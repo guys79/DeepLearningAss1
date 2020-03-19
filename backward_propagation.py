5780d3f4205101291f743553e0f3948ea75f157d
guyschlesinger <guys797@gmail.com>
2020-03-19 18:13:30 +0200

    completed 2.a

--- backward_propagation.py
+++ backward_propagation.py
@@ -1,1 +1,42 @@
-    
+import numpy as np
+"""
+The implementation of the linear part of the backward propagation process for a single layer
+"""
+def Linear_backward(dZ, cache):
+    A_prev = cache[0]
+    W = cache[1]
+    b = cache[2]
+
+    dA_prev = np.dot(W.T,dZ)
+    dW = np.dot(A_prev.T,dZ)
+    db = dZ
+
+    return dA_prev,dW,db
+
+"""
+The implementation for the LINEAR -> ACTIVATION layer. The function first computed dZ and then applies the 
+linear_backward function
+"""
+def linear_activation_backward(dA,cache,activation):
+
+    activation_function = activation[0]
+    Z = activation[1]
+    dZ = 1
+    dA_prev , dW, db = Linear_backward(dZ,cache)
+
+    return dA_prev, dW, db
+
+"""
+This function implements backward propagation for a RELU unit
+"""
+def relu_backward(dA,activation_cache):
+    return "not implemented"
+
+"""
+This function implements backward propagation for a RELU unit
+"""
+def softmax_backward(dA,activation_cache):
+    return "not implemented"
+
+
+
