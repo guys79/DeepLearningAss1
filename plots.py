e1c49e3b992705361d3b09214dbf5cab4d3f9f10
jonmartz <jonmartz91@gmail.com>
2020-04-15 14:39:21 +0200

    implemented plots

--- plots.py
+++ plots.py
@@ -1,15 +1,13 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv('C:\\Users\\Jonathan\\Documents\\BGU\\Semester 8\\courses\\DL\\assignments\\1\\logs.csv')
-
-for metric in ['cost', 'accuracy']:
-    plt.plot(df['epoch'], df[metric], label='without batchnorm')
-    plt.plot(df['epoch'], df['norm_'+metric], label='with batchnorm')
+dir = 'C:\\Users\\Jonathan\\Documents\\BGU\\Semester 8\\courses\\DL\\assignments\\1\\'
+for metric in ['validation cost', 'validation accuracy']:
+    for model in ['normal', 'batchnorm', 'dropout']:
+        df = pd.read_csv('%slog_%s.csv' % (dir, model))
+        plt.plot(df['epoch'], df[metric], label=model)
     plt.legend()
     plt.xlabel('epoch')
     plt.ylabel(metric)
-    # plt.title('Convergence progression')
-
-    plt.savefig('C:\\Users\\Jonathan\\Documents\\BGU\\Semester 8\\courses\\DL\\assignments\\1\\'+metric+'.png')
+    plt.savefig('%s%s.png' % (dir, metric))
     plt.show()
